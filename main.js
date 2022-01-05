1db74d939479d3ecad180399db1982370e0488a7
Moncef-Tighi <monceftighiouart@gmail.com>
2022-01-05 13:04:25 +0100

    setting up socket.io initial connection

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+const http= require("http");
 const express = require("express");
+const socketio= require("socket.io");
+
 
 const server= express();
-
-
+//C'est bizarre comme logique mais Socket.io en a besoin et fait ça en background sinon.
+const app = http.createServer(server);
+const io = socketio(app);
 
 
 
 
 server.use(express.static("./public"));
 
+io.on("connection", (socket)=> {
+    console.log(socket);
+})
 
 
-module.exports= server;
+module.exports= app;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const server = require("./main");
+const app = require("./main");
 const dotenv= require('dotenv')
 dotenv.config({path:__dirname+'/config.env'});
 
 
 const port = process.env.PORT || 2000
-server.listen(port, ()=> {
+app.listen(port, ()=> {
     console.log(`Server open on port ${port}`);
 });
